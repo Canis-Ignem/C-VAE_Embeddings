1102a942482aeda58bfcaa659c9d8876220e8650
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-07-24 02:25:56 +0200

    more output shape changes

--- model.py
+++ model.py
@@ -54,6 +54,6 @@ class Decoder(Module):
     def forward(self, x):
         
         x = self.decode(x)
-        x = x.view(-1, 1, self.vocab_size, 1)
+        x = x.view(-1, 1, self.vocab_size)
         return x
     
