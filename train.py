1a1959ff0e17f112f38b0529d8a9d6e67ccedc1f
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-07-24 14:38:22 +0200

    sample no cuda

--- train.py
+++ train.py
@@ -40,7 +40,7 @@ for epoch in range(epochs):
     decoder.train()
 
     for i in tqdm(range(0, train.size(0) - 1)):
-        prior = D.Normal(torch.zeros(512, ), torch.ones(512,)).to(device)
+        prior = D.Normal(torch.zeros(512, ), torch.ones(512,))
         x , y = dh.get_batch(train, i)
         
         input = torch.zeros( (1,len(vocab), 1) )
@@ -93,7 +93,7 @@ for epoch in range(epochs):
             
             for i in tqdm(range(0, val.size(0) - 1)):
                 
-                prior = D.Normal(torch.zeros(512, ), torch.ones(512,)).to(device)
+                prior = D.Normal(torch.zeros(512, ), torch.ones(512,))
                 x , y = dh.get_batch(val, i)
                 
                 input = torch.zeros( (1,len(vocab), 1) )
