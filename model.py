3887398499f9fcbe909fdf8b9e4b77a55f304a7b
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-07-24 23:15:16 +0200

    shapes

--- model.py
+++ model.py
@@ -111,10 +111,11 @@ class Decoder(Module):
     def forward(self, x):
         
         x = self.decode(x)
+        print(x.shape)
         x = x.view(-1, self.vocab_size,1)
         return x
 
  
 d = Decoder(28782,512)
 
-print(summary(d,(1,20,512)))
+print(summary(d,(1,512)))
