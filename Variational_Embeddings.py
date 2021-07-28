0f835479f7e06c394a84f706403930e7fc95bb7f
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-07-28 22:19:49 +0200

    testing sentence embeddings

--- Variational_Embeddings.py
+++ Variational_Embeddings.py
@@ -1,5 +1,4 @@
-from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, Conv1d, MaxPool2d, Module, Softmax, BatchNorm1d, Dropout, LeakyReLU, Sigmoid
-from torchtext.data.functional import sentencepiece_tokenizer, generate_sp_model
+from torchtext.data.utils import get_tokenizer
 import torch.nn.functional as F
 import torch.distributions as D
 from torch import nn
@@ -11,15 +10,11 @@ from torchsummary import summary
 
 
 
-
+tokenizer = get_tokenizer("basic_english")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 ENCODER = torch.load("models/encoder.pth").to(device)
 VOCAB = torch.load("models/vocab.pth").to(device)
-
-spm = generate_sp_model(".data/WikiText2/wikitext-2-v1.zip", vocab_size= len(VOCAB), model_type="word")
-
-sp_tokens_generator = sentencepiece_tokenizer(spm)
 
 
 def encode_word(word):
@@ -43,24 +38,23 @@ sentences = ["I like to eat", "I like to drink", "I like to sleep", "I like to p
 
 
 
-'''
-def encode_word(sentence):
-    
-    
+
+def encode_sentence(sentence):
     prior = D.Normal(torch.zeros(512,).to(device), torch.ones(512,).to(device))
-    x = torch.zeros( (2,len(VOCAB), 1) ).to(device)
+    words = tokenizer(sentence)
     
-    x[0][VOCAB[word]][0] = 1
+    x = torch.zeros( (len(words),len(VOCAB), 1) ).to(device)
+    for i in range(len(words)):
+        x[i][VOCAB[words[i]]][0] = 1
     out = ENCODER(x)
-    
-    z_mu = out[0, 0, :]
-    z_logvar = out[0, 1, :]         
+    z_mu = out[:, 0, :]
+    z_logvar = out[:, 1, :]         
     epsilon = prior.sample()
     
     z = z_mu.to(device) + epsilon.to(device) * (z_logvar.to(device) / 2).exp()
     return z
 
 
-print(encode_word("cat").size())
-''' 
+print(encode_sentence("Hello! Good morning").size())
+
     
