34a9382cbf89e80a5c52d263a37251d16fac6dc7
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2021-07-28 21:22:38 +0200

    typo

--- Variational_Embeddings.py
+++ Variational_Embeddings.py
@@ -11,7 +11,7 @@ VOCAB = torch.load("models/vocab.pth")
 
 def encode_word(word):
     
-    x = torch.tensor((1,len(VOCAB,1))).cuda()
+    x = torch.tensor( (1, len(VOCAB),1 ) ).cuda()
     x[0][VOCAB[word]][0] = 1
     out = ENCODER(x)
     return out
